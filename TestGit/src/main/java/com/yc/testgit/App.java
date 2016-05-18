26b54edcf81d1fa5b4646cb7d08e74642f368103
rdzgit <2285458934@qq.com>
2016-05-18 20:59:47 +0800

    4

--- TestGit/src/main/java/com/yc/testgit/App.java
+++ TestGit/src/main/java/com/yc/testgit/App.java
@@ -7,7 +7,7 @@ package com.yc.testgit;
 public class App {
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
-        System.out.println("1");
+        System.out.println(1);
         System.out.println(2);
     }
 }
