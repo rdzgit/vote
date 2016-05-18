4985fc9fab138752ab31d275d6240d9a922a0594
rdzgit <2285458934@qq.com>
2016-05-18 20:31:22 +0800

    第三次修改 2.0

--- TestGit/src/main/java/com/yc/testgit/App.java
+++ TestGit/src/main/java/com/yc/testgit/App.java
@@ -4,11 +4,10 @@ package com.yc.testgit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
         System.out.println("1");
+        System.out.println(2);
     }
 }
